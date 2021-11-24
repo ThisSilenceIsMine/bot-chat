87ed2b54d04fb9d8782b5d8d414d70f1b65dbe60
ThisSilenceIsMine <aurakel542@gmail.com>
2021-11-24 14:28:47 +0200

    Header plus basic styling

---
+++ apps/client/components/header/header.tsx
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <H>Chat Bots 2.0</H>
+    </StyledHeader>
+  );
+};
+
+export default Header;
+
+const StyledHeader = styled.header`
+  display: flex;
+  width: 100%;
+  height: 80px;
+  background-color: white;
+`;
+
+const H = styled.h1`
+  font-size: 32px;
+  font-family: 'Open Sans', Arial, sans-serif;
+  font-weight: 600;
+  margin-left: 80px;
+  margin-top: calc(60px - 1em);
+`;
--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,20 +1,17 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import Header from 'components/header/header';
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to client!</title>
+        <title>Welcome to Bot-Chat!</title>
       </Head>
       <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to client!</h1>
-        </header>
-        <main>
+        <Header />
+        <main className="main-wrapper">
           <Component {...pageProps} />
         </main>
       </div>
