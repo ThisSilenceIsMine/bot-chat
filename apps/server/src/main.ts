7f5e23e8d83fbb199597aa056a62771fe8d8e417
ThisSilenceIsMine <aurakel542@gmail.com>
2021-11-25 21:47:46 +0200

    Basic server io. Created lib for shared types

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,18 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
+import * as http from 'http';
+import { Server } from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to Server!' });
+  res.sendFile(__dirname + '/assets/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 const port = process.env.port || 3333;
-const server = app.listen(port, () => {
+
+server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
